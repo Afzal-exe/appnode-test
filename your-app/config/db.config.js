421beeb3b0316624f8453aeb207adfbe7ce719a5
Mohammed Afzal <108460168+Afzal-exe@users.noreply.github.com>
2024-03-17 15:36:19 +0530

    Delete your-app directory

--- your-app/config/db.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    host: 'your_rds_endpoint',
-    user: 'admin',
-    password: '12345678',
-    database: 'your_database_name'
-  };
-  
--- your-app/models/user.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose'); // Assuming Mongoose for interacting with database (replace with your ORM)
-
-const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
-});
-
-module.exports = mongoose.model('User', userSchema);
